ef1d6bd36303841438315b95060ae365f58fe884
RITIK JAIN <33231793+ritikjain1907@users.noreply.github.com>
2022-06-12 12:36:30 +0530

    Update Validator.java

--- 3_Larger_Number/src/main/java/validator/Validator.java
+++ 3_Larger_Number/src/main/java/validator/Validator.java
@@ -11,15 +11,15 @@ package main.java.validator;
 public class Validator {
 	
 	/**
-	 * validate method to validate if the input numbers are not equal
-	 * @param inputInteger
-	 * @return Boolean.True - Valid case
-	 * @return NumberFormatException - Invalid case
+	 * @description - validate method to validate if the input numbers are equal or not
+	 * @param firstNumber
+	 * @param secondNumber
+	 * @return Boolean
 	 */
 	public Boolean validate(double firstNumber, double secondNumber) {
 		
 		if(firstNumber == secondNumber) {
-			throw new ArithmeticException("Entered numbers are equal to each other");
+			throw new RuntimeException("Entered numbers are equal to each other");
 		}
 		
 		return Boolean.TRUE;
